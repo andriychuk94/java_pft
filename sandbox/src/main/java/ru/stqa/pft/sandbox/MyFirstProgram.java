2d20329c3544a49a50a3bc69e2899934fbec5d03
andriychuk94 <andriychuk23@gmail.com>
2017-06-07 23:35:16 +0300

    Добавлено декілька функцій

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,31 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
-    }
+
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Boris");
+
+    double l = 5;
+    System.out.println("Площа квадрата зі стороною " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площа прямокутника зі сторонами " + a + " і " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+    /*Для повернення результату потрібно написати return тип повертання результату вказується перед назвою
+     * функцію в нашому випадку  public static тип повернення результату  !!double!! тобто число (5.0...100.0)*/
+  }
+
+  public static double area(double a, double b) {
+    return a*b;
+  }
 }
